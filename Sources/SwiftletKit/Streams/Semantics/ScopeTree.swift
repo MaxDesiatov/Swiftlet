beaa7cfd23d8f1113850850abf8b621b90a8477b
Max Desiatov <max@desiatov.com>
2018-09-19 21:33:02 +0100

    Improved scope tree declarations

--- Sources/SwiftletKit/Streams/Semantics/ScopeTree.swift
+++ Sources/SwiftletKit/Streams/Semantics/ScopeTree.swift
@@ -7,55 +7,103 @@
 
 import Foundation
 
-enum InheritableType {
-  case `protocol`
-  case `class`(name: String, generics: [String])
+protocol Type {
 }
 
-struct ClassScope {
-  let name: String
-  let genericParameters: [String]
-  let inheritedTypes: [InheritableType]
-}
-
-indirect enum Type {
-  case closure(argument: Type, returns: Type, throws: Bool)
-  case tuple([Type])
+indirect enum ConcreteType: Type {
+  case closure(argument: ConcreteType, returns: ConcreteType, throws: Bool)
+  case tuple([ConcreteType])
   case instanceType(name: String, generics: [String])
 }
 
+enum PlaceholderType: Type {
+  case explicit(ConcreteType)
+  case notInferred
+}
+
 struct ProtocolFunction {
-  let name: String
   let genericParameters: [String]
-  let parameters: [String: Type]
-  let returnType: Type?
+  let parameters: [String: ConcreteType]
+  let returnType: ConcreteType
 }
 
-final class ProtocolScope {
-  weak var parent: FileScope!
+enum FunctionScopeParent<T: Type> {
+  case function(FunctionScope<T>)
+  case `class`(ClassScope<T>)
+  case `struct`(StructScope<T>)
+}
 
-  let associatedTypes = [String]()
-  let functions = [ProtocolFunction]()
+final class FunctionScope<T: Type> {
+  let parent: FunctionScopeParent<T>
 
-  init(parent: FileScope) {
+  var functions = [String: FunctionScope<T>]()
+
+  init(parent: FunctionScopeParent<T>) {
     self.parent = parent
   }
 }
 
-final class FileScope {
-  weak var parent: ModuleScope!
+enum ObjectScopeParent<T: Type> {
+  case file(FileScope<T>)
+  case `class`(ClassScope<T>)
+  case `struct`(StructScope<T>)
+}
 
-  let imports = [String]()
-  let protocols = [String: ProtocolScope]()
-  let classes = [String: ClassScope]()
+final class StructScope<T: Type> {
+  let parent: ObjectScopeParent<T>
 
-  init(parent: ModuleScope) {
+  var conformanceTypes = [String]()
+
+  init(parent: ObjectScopeParent<T>) {
     self.parent = parent
   }
 }
 
-final class ModuleScope {
-  let files = [String: FileScope]()
+final class ProtocolScope<T: Type> {
+  weak var parent: FileScope<T>!
+
+  var associatedTypes = [String]()
+  var functions = [String: ProtocolFunction]()
+
+  init(parent: FileScope<T>) {
+    self.parent = parent
+  }
+}
+
+enum InheritableType {
+  case `protocol`(name: String)
+  case `class`(name: String, generics: [String])
+}
+
+final class ClassScope<T: Type> {
+  let parent: ObjectScopeParent<T>
+
+  var genericParameters = [String]()
+  var inheritedTypes = [InheritableType]()
+  var constants = [String: T]()
+  var variables = [String: T]()
+
+  init(parent: ObjectScopeParent<T>) {
+    self.parent = parent
+  }
+}
+
+final class FileScope<T: Type> {
+  weak var parent: ModuleScope<T>!
+
+  var imports = [String]()
+  var protocols = [String: ProtocolScope<T>]()
+  var classes = [String: ClassScope<T>]()
+  var constants = [String: T]()
+  var variables = [String: T]()
+
+  init(parent: ModuleScope<T>) {
+    self.parent = parent
+  }
+}
+
+final class ModuleScope<T: Type> {
+  let files = [String: FileScope<T>]()
 }
 
 enum DeclarationType {
